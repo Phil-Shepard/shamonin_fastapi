cb47394f0399f60ba78073fb2e7ee531eadff447
Phil-Shepard <Philipp-74@yandex.ru>
2024-06-18 14:04:23 +0500

    Initial commit

--- main.py
+++ main.py
@@ -19,4 +19,7 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(tasks_router)
 
 
+if __name__ == '__main__':
+    import uvicorn
 
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
